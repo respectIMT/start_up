8c3439c193804a64c3770a5fe84b5346b8cfdde2
Muhsinjon Ibrohimov <muhsinjonibrohimov2002@gmail.com>
2023-09-05 15:18:12 +0500

    add

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:start_up/main/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: ScreenUtilInit(
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (context, child) => const MainScreen()),
+      ),
+    );
+  }
+}
